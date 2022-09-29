38cda6727fa57e5e4d6323594374f926b0dfbc15
Alex Degregori <alexdegregori@gmail.com>
2022-09-29 09:59:53 -0700

    File adjustment

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
-var indexRouter = require("./routes/index");
+var indexRouter = require("./index");
 
 var app = express();
 
@@ -22,17 +22,17 @@ app.use("/", indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-	next(createError(404));
+  next(createError(404));
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-	// set locals, only providing error in development
-	res.locals.message = err.message;
-	res.locals.error = req.app.get("env") === "development" ? err : {};
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
-	// render the error page
-	res.status(err.status || 500);
+  // render the error page
+  res.status(err.status || 500);
 });
 
 module.exports = app;
